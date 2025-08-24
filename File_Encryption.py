ada1dfdddb643876250996b3e775cf10061c94f3
Jackson Lee <jacksonlee@Jacksons-MBP-2.lan>
2025-08-24 22:11:59 +1000

    file revised

--- File_Encryption.py
+++ File_Encryption.py
@@ -27,26 +27,25 @@ def unscramble(data, key):
 
 def encrypt_file(input_path, output_path, key):
     with open(input_path, "rb") as f_in:
-        data = f_in.read()
-    scrambled = scramble(data, key)
+        data = f_in.read() # read the data from the file and assign it to the variable: data
+    scrambled = scramble(data, key) # transposition the data with the key value input
     with open(output_path, "wb") as f_out:
-        f_out.write(scrambled)
+        f_out.write(scrambled) # save the scrambled data into a file
     print(f"File encrypted and saved as {output_path}")
 
 def decrypt_file(input_path, output_path, key):
     with open(input_path, "rb") as f_in:
-        data = f_in.read()
-    unscrambled = unscramble(data, key)
+        data = f_in.read() # read the scrambled data from the file
+    unscrambled = unscramble(data, key) # reverse the scrambling process
     with open(output_path, "wb") as f_out:
-        f_out.write(unscrambled)
+        f_out.write(unscrambled) # output the unscrambled data into a file
     print(f"File decrypted and saved as {output_path}")
 
-# Example usage:
+
 def main_scramble():
     filepath = "C:\\Hash\\"
     filename = input("Enter the filename: ").strip()
     filename = filepath + filename
-    #key = 5  # simple shift key
     key = int(input("Enter key for encryption: "))
     #encrypt_file("original.txt", "encrypted.bin", key)
     #decrypt_file("encrypted.bin", "decrypted.txt", key)
--- Password_Strength_Meter.py
+++ Password_Strength_Meter.py
@@ -5,7 +5,7 @@ def password_strength(pw: str):
     COMMON_PASSWORDS = [
         "password", "123456", "qwerty", "abc123", "letmein",
         "admin", "welcome", "password123", "iloveyou", "monkey"
-    ]
+    ] # a list of common password people like to choose
 
     length = len(pw)
     is_common = pw.lower() in COMMON_PASSWORDS
@@ -13,28 +13,26 @@ def password_strength(pw: str):
     # very simple repetition/predictability check
     #repeated = length >= 6 and (pw == pw[0] * length or pw in ("abcdef", "123456", "qwerty"))
 
-    score = 0
-    if length >= 8: score += 1
-    if length >= 12: score += 1
-    if any(c.islower() for c in pw): score += 1
-    if any(c.isupper() for c in pw): score += 1
-    if any(c.isdigit() for c in pw): score += 1
-    if any(not c.isalnum() for c in pw): score += 1
-    if is_common: score -= 2
-    #if repeated: score -= 1
+    score = 0 # Initialize the score as 0
+    if length >= 8: score += 1 # password with length greater or equal to 8, add 1 score
+    if length >= 12: score += 1 # password with length greater or equal to 12, add an additional score
+    if any(c.islower() for c in pw): score += 1 # include any lowercase letter, add 1 score
+    if any(c.isupper() for c in pw): score += 1 # include any uppercase letter, add 1 score
+    if any(c.isdigit() for c in pw): score += 1 # include any numeric value, add 1 score
+    if any(not c.isalnum() for c in pw): score += 1 # include any special character other than alphanumeric value, add 1 score
+    if is_common: score -= 2 # match wih any common password, minus 2 score
 
-    if score <= 2: label = "very weak"
-    elif score == 3: label = "weak"
-    elif score == 4: label = "okay"
-    elif score == 5: label = "strong"
-    else: label = "very strong"
+    if score <= 2: label = "very weak" # score less or equal to 2, classify as very weak password
+    elif score == 3: label = "weak" # score equal to 3, classify as weak password
+    elif score == 4: label = "okay" # score equal to 4, classify as okay password
+    elif score == 5: label = "strong" # score equal to 5, classify as strong password
+    else: label = "very strong" # score greater than 5, classify as very strong password
 
     return label
 
 def password_meter():
-    password = input("Enter Your Password for Assessment: ")
-    #password_strength(password)
-    print("Your Password Strength is", password_strength(password))
+    password = input("Enter Your Password for accessing its strength: ") # let user to input the password for assessment
+    print("Your Password Strength is", password_strength(password))  # compute the score of password and return the strength of password
 
 # Run main if this script is executed
 #if __name__ == "__main__":
